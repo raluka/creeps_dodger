10288a34d7149c84a903c24c63504c987a15cb9f
raluka <raluca@tng.sh>
2026-07-10 18:12:28 +0200

    Add Main scene

---
+++ main.gd
@@ -0,0 +1,43 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate() # create new mob instance
+	
+	# place it at random position
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	mob.position = mob_spawn_location.position
+	
+	# set the mob direction 
+	var direction = mob_spawn_location.rotation + PI / 2
+	direction += randf_range(-PI / 4, PI / 4)
+	
+	# set velocity of the mob
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	# spawn the mob by adding it to main scene
+	add_child(mob)
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
