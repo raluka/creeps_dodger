c53a0af3d4d0edb670707e7da78c366349791a89
raluka <raluca@tng.sh>
2026-07-13 11:07:22 +0200

    Connect HUD with Main

--- main.gd
+++ main.gd
@@ -10,11 +10,14 @@ func _ready():
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate() # create new mob instance
@@ -37,6 +40,7 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
