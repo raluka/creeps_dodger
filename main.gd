3e0505f6170d2910af5bf3daa4672e039aa7883f
raluka <raluca@tng.sh>
2026-07-13 11:29:13 +0200

    Add sound resources

--- main.gd
+++ main.gd
@@ -11,6 +11,8 @@ func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -19,6 +21,7 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate() # create new mob instance
